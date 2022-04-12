f2b3a9113f22cf91e9ee978a380032af4df4cccd
Glitch (thinkable-sage-existence) <none>
2022-04-12 04:36:29 +0000

    🐪🫑 Updated with Glitch

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 Submitted by: **Maurice August**
 
-Time spent: **6** hours spent in total
+Time spent: **8** hours spent in total
 
 Link to project: https://glitch.com/edit/#!/thinkable-sage-existence
 
@@ -30,7 +30,7 @@ The following **optional** features are implemented:
 * [X] Player only loses after 3 mistakes (instead of on the first mistake)
 * [ ] Game button appearance change goes beyond color (e.g. add an image)
 * [ ] Game button sound is more complex than a single tone (e.g. an audio file, a chord, a sequence of multiple tones)
-* [ ] User has a limited amount of time to enter their guess on each turn
+* [X] User has a limited amount of time to enter their guess on each turn
 
 The following **additional** features are implemented:
 
@@ -40,6 +40,7 @@ The following **additional** features are implemented:
 
 ## Video Walkthrough (GIF)
 
+![](https://user-images.githubusercontent.com/85422454/159787660-f531e9a2-f855-4bb6-bb63-737ca232af7d.gif)
 ![](https://user-images.githubusercontent.com/85422454/159141421-3cd59169-c024-44ba-aa8d-dcf6bdad2b32.gif)
 ![](https://user-images.githubusercontent.com/85422454/159141439-033b48e4-8ccf-4261-a34f-2d0464134682.gif)
 ![](https://user-images.githubusercontent.com/85422454/159141444-5eafad38-c24a-4b6f-ae0f-6fdd4a6d6147.gif)
@@ -52,19 +53,19 @@ The following **additional** features are implemented:
 
 2. What was a challenge you encountered in creating this submission (be specific)? How did you overcome it? (recommended 200 - 400 words)
   
-  Something that was tricky for me while completing this project was the debugging differences between back-end vs front-end languages, particularly when it comes to JavaScript. Most of my experience lies with Java, and if I were to run into an error with my code I am used to either printing out the logic with System.out.print(), testing breakpoints, or using the rest of the debugging tool provided by IDEs like Intellij or Eclipse. However, with front-end development, specifically with CSS and Javascript, I have not learned of a debugging tool that works similarly to Java or Python in an IDE. Most of my attempts at debugging were just trial and error replacing old code with new code and hoping that it looks or sounds good on the preview screen. Moreover, JavaScript offers the best of both worlds in being able to communicate with markup languages using the DOM and offers back-end structures like loops and decision branches; still, to debug my code the quickest solution that I know of is to use console.log() and inspect the screen. There are much more steps involved and using the developer view is not always reliable for me. The Safari inspect element function and console in the developer screen were completely gone for me during the project, and when it finally worked in Chrome, the JavaScript variables and functions would not appear if I did not inspect the preview screen in Glitch.
+  Something that was tricky for me while completing this project was the debugging differences between back-end vs front-end languages, particularly when it comes to JavaScript. Most of my experience lies with Java, and if I were to run into an error with my code I am used to either printing out the logic with System.out.println(), testing breakpoints, or using the rest of the debugging tools provided by IDEs like Intellij or Eclipse. However, with front-end development, specifically with CSS and Javascript, I have not yet learned of a debugging tool that works similarly to Java or Python IDEs. Most of my attempts at debugging were just trial and error replacing old code with new code and hoping that it looks or sounds good on the preview screen. Moreover, JavaScript offers the best of both worlds in being able to communicate with markup languages using the DOM and offers back-end structures like loops and decision branches; still, to debug my code the quickest solution that I know of is to use console.log() and inspect the screen. There are much more steps involved and using the developer view is not always reliable for me. The Safari inspect element function and console in the developer screen were completely gone for me during the project, and when it finally worked in Chrome, the JavaScript variables and functions would not appear if I did not inspect the preview screen in Glitch.
 
 3. What questions about web development do you have after completing your submission? (recommended 100 - 300 words) 
 
-One thing that I am curious about after working on this project is some ways to debug front-end code since the output is mostly visual. As mentioned in the things I found challenging about this project, I am more used to the debugging process of back-end IDEs, where common methods involve print statements or using breakpoints for the debugger tool. With front-end code, the process is a little different. When I first learned web development using HTML and CSS, much of the styling and element alignment was done with trial and error. I wanted to know if there was a more efficient way of doing this. Other similar questions are: are there debugging packages that can be installed in a text editor like Atom or Glitch? Is there a traditional IDE like Eclipse for front-end work?
+One thing that I am curious about after working on this project is how to debug front-end code since the output is mostly visual. As mentioned in the things I found challenging about this project, I am more used to the debugging process of back-end IDEs, where common methods involve print statements or using breakpoints for the debugger tool. With front-end code, the process is different. When I first learned web development using HTML and CSS, much of the styling and element alignment was done with trial and error. I wanted to know if there was a more efficient way of doing this. Other similar questions are: are there debugging packages that can be installed in a text editor like Atom or Glitch? Is there a traditional IDE like Eclipse for front-end work?
 
-I was also curious about the languages/frameworks used in vibrant, mostly software service websites. Websites like codepen.io, algoexpert.io, and CSS-tricks.com showcase bold, vibrant elements that can be interacted with by the user. I know that there are popular frameworks used to make web development easier but do all of the frameworks have the code needed to create this? 
+I was also curious about the languages/frameworks used in vibrant, mostly software service websites. Websites like codepen.io, algoexpert.io, and CSS-tricks.com showcase bold, vibrant elements that can be interacted with by the user. I know that there are popular frameworks used to make web development easier but do all of the frameworks have the code needed to create this or is it really up to the programmer on how to combine all of them? 
 
 4. If you had a few more hours to work on this project, what would you spend them doing (for example: refactoring certain functions, adding additional features, etc). Be specific. (recommended 100 - 300 words) 
 
 If I had more time, I would begin working on a score counter so that every time the user wins, the wins accumulate, and a "best score" history is generated. This would most likely involve creating a score counter variable to keep track of the user reaching the end of the sequence and winning, a local storage or an array that keeps track of the history of scores, and a function that is called at the end of the game to compare the current win to the history of scores and checks if a new record has been reached. 
 
-In continuation of the first one, another feature I would add is the ability to get scores of other users and compare them to your own. I would imagine this being more work to build, but a possible strategy could involve making the website public, storing the wins from other users in a database using a language like SQL, uploading the user's wins to the database, and either checking to see who has the highest score relative to that user's win rank(1-10, 11-20, etc.) or at the end of the day pausing the collection of new scores in the database to go back and check the previous ones for the user with the most wins. 
+In continuation of the first one, another feature I would add is the ability to get scores of other users and compare them to your own. I would imagine this being more work to build, but a possible strategy could involve making the website public, storing the wins from other users in a database using a language like SQL, uploading the user's wins to the database, and either checking to see who has the highest score relative to that user's win rank(1-10, 11-20, etc.), or at the end of the day pausing the collection of new scores in the database to go back and check the previous ones for the user with the most wins. 
 
 ## Interview Recording URL Link
 
--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 // global constants
 const cluePauseTime = 333; //how long to pause in between clues
 const nextClueWaitTime = 1000; //how long to wait before starting playback of the clue sequence
+const countdownElement = document.getElementById("countdown");
+const time = 4;
+const timeElement = document.getElementById("timer");
 
 // global variables
 var clueHoldTime = 1000; //how long to hold each clue's light/sound
 var tonePlaying = false;
-var volume = 0.5;  //must be between 0.0 and 1.0
+var volume = 0.5; //must be between 0.0 and 1.0
 var pattern = [8, 2, 4, 3, 7, 6, 5, 4];
-var progress = 0; 
+var progress = 0;
 var gamePlaying = false;
 var guessCounter = 0;
 var patternLen = pattern.length;
@@ -16,215 +19,236 @@ var mistakesLeft;
 var hardmodeActivate;
 var record = [];
 var position = 0; //keeps track of win array position
+let seconds = time * 60;
+var interval;
 
-function randomPattern(){
+// timer function
+function startTimer(){
+  const minutes = Math.floor(seconds / 60);
+  let remainingSecs = seconds % 60;
+  remainingSecs = remainingSecs < 10 ? '0' + remainingSecs:remainingSecs;
   
+  if(seconds == 240){
+    timeElement.innerHTML = `${minutes}:00`;
+  }
+  else{
+    timeElement.innerHTML = `${minutes}:${remainingSecs}`;
+  }
+  
+  if(gamePlaying == true && seconds == 0){
+    
+    loseGame();
+  }
+  
+  seconds--;
+}
+
+
+// generate random pattern
+function randomPattern() {
   let i = 0;
   var newPattern = [];
-  
+
   //fill new array with random numbers
-  for (i = 0; i < pattern.length; i++){
-    
+  for (i = 0; i < pattern.length; i++) {
     newPattern[i] = Math.floor(Math.random() * 8) + 1;
-  
   }
- 
+
   return newPattern;
 }
 
 var testPattern = randomPattern();
 
-function startGame(){
+// game functions
+function startGame() {
+  pattern = randomPattern();
+  playClueSequence();
 
-    pattern = randomPattern();
-    playClueSequence();
+  //initialize game variables
+  progress = 0;
+  gamePlaying = true;
+  mistakes = 0;
+  mistakesLeft = 3;
+  clueHoldTime = 1000;
+
+  // swap the Start and Stop buttons
+  document.getElementById("startBtn").classList.add("hidden");
+  document.getElementById("stopBtn").classList.remove("hidden");
   
-    //initialize game variables
-    progress = 0;
-    gamePlaying = true;
-    mistakes = 0;
-    mistakesLeft = 3;
-    clueHoldTime = 1000;
+  //make timer visible
+  timeElement.classList.remove("hidetimer");
   
-    // swap the Start and Stop buttons
-    document.getElementById("startBtn").classList.add("hidden");
-    document.getElementById("stopBtn").classList.remove("hidden");
-  
+  interval = setInterval(startTimer, 1000);
 }
 
-function stopGame(){
-    gamePlaying = false;
-    progress = 0;
-    mistakes = 0;
-    mistakesLeft = 3;
-    guessCounter = 0;
+function stopGame() {
   
-    // swap the Start and Stop buttons
-    document.getElementById("stopBtn").classList.add("hidden");
-    document.getElementById("startBtn").classList.remove("hidden");
+  // stop and reset timer
+  clearInterval(interval);
+  seconds = time * 60;
+  
+  // hide timer
+  timeElement.classList.add("hidetimer");
+  
+  gamePlaying = false;
+  progress = 0;
+  mistakes = 0;
+  mistakesLeft = 3;
+  guessCounter = 0;
+
+  // swap the Start and Stop buttons
+  document.getElementById("stopBtn").classList.add("hidden");
+  document.getElementById("startBtn").classList.remove("hidden");
 }
 
 // Sound Synthesis Functions
 const freqMap = {
-  1: 261.6,
+  1: 250,
   2: 329.6,
   3: 398,
   4: 462.2,
   5: 350,
   6: 400,
   7: 290,
-  8: 300
-}
+  8: 300,
+};
 
-function playTone(btn,len){ 
-  o.frequency.value = freqMap[btn]
-  g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
-  context.resume()
-  tonePlaying = true
-  setTimeout(function(){
-    stopTone()
-  },len)
+function playTone(btn, len) {
+  o.frequency.value = freqMap[btn];
+  g.gain.setTargetAtTime(volume, context.currentTime + 0.05, 0.025);
+  context.resume();
+  tonePlaying = true;
+  setTimeout(function () {
+    stopTone();
+  }, len);
 }
-function startTone(btn){
-  if(!tonePlaying){
-    context.resume()
-    o.frequency.value = freqMap[btn]
-    g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
-    context.resume()
-    tonePlaying = true
+function startTone(btn) {
+  if (!tonePlaying) {
+    context.resume();
+    o.frequency.value = freqMap[btn];
+    g.gain.setTargetAtTime(volume, context.currentTime + 0.05, 0.025);
+    context.resume();
+    tonePlaying = true;
   }
 }
-function stopTone(){
-  g.gain.setTargetAtTime(0,context.currentTime + 0.05,0.025)
-  tonePlaying = false
+function stopTone() {
+  g.gain.setTargetAtTime(0, context.currentTime + 0.05, 0.025);
+  tonePlaying = false;
 }
 
-function lightButton(btn){
-  document.getElementById("button"+btn).classList.add("lit")
+function lightButton(btn) {
+  document.getElementById("button" + btn).classList.add("lit");
 }
-function clearButton(btn){
-  document.getElementById("button"+btn).classList.remove("lit")
+function clearButton(btn) {
+  document.getElementById("button" + btn).classList.remove("lit");
 }
 
-function playSingleClue(btn){
-  if(gamePlaying){
+function playSingleClue(btn) {
+  if (gamePlaying) {
     lightButton(btn);
-    playTone(btn,clueHoldTime);
-    setTimeout(clearButton,clueHoldTime,btn);
+    playTone(btn, clueHoldTime);
+    setTimeout(clearButton, clueHoldTime, btn);
   }
 }
 
-function playClueSequence(){
+function playClueSequence() {
   guessCounter = 0;
-  context.resume()
+  context.resume();
   let delay = nextClueWaitTime; //set delay to initial wait time
-  for(let i=0;i<=progress;i++){ // for each clue that is revealed so far
-    console.log("play single clue: " + pattern[i] + " in " + delay + "ms")
-    setTimeout(playSingleClue,delay,pattern[i]) // set a timeout to play that clue
-    
-    //make adjust clue time based on easy vs hard mode
-    if(hardmodeActivate == true){
-      clueHoldTime -= 25;
+  for (let i = 0; i <= progress; i++) {
+    // for each clue that is revealed so far
+    console.log("play single clue: " + pattern[i] + " in " + delay + "ms");
+    setTimeout(playSingleClue, delay, pattern[i]); // set a timeout to play that clue
+
+    //adjust clue time based on easy vs hard mode
+    if (hardmodeActivate == true) {
+      clueHoldTime -= 27;
+    } else {
+      clueHoldTime -= 14;
     }
-    else{
-      clueHoldTime -= 20;
-    }
-    
-    delay += clueHoldTime 
+
+    delay += clueHoldTime;
     delay += cluePauseTime;
   }
 }
 
-function loseGame(){
+function loseGame() {
   stopGame();
   alert("Game Over. You lost.");
 }
 
-function winGame(){
+function winGame() {
   stopGame();
   alert("Congratulations, you won!");
 }
 
-function guess(btn){
+function guess(btn) {
   let alertMssg = "";
-  
+
   console.log("user guessed: " + btn);
-  if(!gamePlaying){
+  if (!gamePlaying) {
     return;
   }
   // add game logic here
-  if(btn == pattern[guessCounter]){
-    if(progress == guessCounter){
-      if(progress == patternLen - 1){
+  if (btn == pattern[guessCounter]) {
+    if (progress == guessCounter) {
+      if (progress == patternLen - 1) {
         newRecord(1);
         winGame();
-      }
-      else{
+      } else {
         progress++;
         playClueSequence();
       }
-    }
-    else{
+    } else {
       guessCounter++;
     }
-  }
-  else if(btn != pattern[guessCounter] && mistakes < 3){
-    
+  } else if (btn != pattern[guessCounter] && mistakes < 3) {
     mistakes++;
-    
-    if(document.getElementById("hard").checked){
-      hardMode();
-    }
-    else{
-      // alert for number of mistakes left
-      if (mistakesLeft >= 0){
 
+    if (document.getElementById("hard").checked) {
+      hardMode();
+    } else {
+      // alert for number of mistakes left
+      if (mistakesLeft >= 0) {
         mistakesLeft -= 1;
 
         alertMssg = "You have " + mistakesLeft + " mistakes left";
         alert(alertMssg);
-
       }
     }
-    
+
     playClueSequence();
-    
-  }
-  else{
+  } else {
     loseGame();
   }
 }
 
 // for hard mode
-function hardMode(){
+function hardMode() {
   let mssgAlert = "";
 
   // alert for number of mistakes left
-    if (mistakes == 1){
-      
-      mssgAlert = "You have 0 mistakes left";
-      alert(mssgAlert);
-      
-    } 
-  
-    if(mistakes == 2){
-      loseGame();
-    }
+  if (mistakes == 1) {
+    mssgAlert = "You have 0 mistakes left";
+    alert(mssgAlert);
   }
 
-//keep track of wins 
-function newRecord(newWin){
-  
+  if (mistakes == 2) {
+    loseGame();
+  }
+}
+
+//keep track of wins
+function newRecord(newWin) {
   var wins;
-  
+
   record[position] = newWin;
-  
+
   wins = record.length;
-  
+
   position++;
-  
+
   return wins;
-  
 }
 
 //test wins are incremented in record array
@@ -232,14 +256,13 @@ var collectwins = newRecord(1);
 var sf = newRecord(1);
 var tg = newRecord(1);
 
-
 // Page Initialization
 // Init Sound Synthesizer
-var AudioContext = window.AudioContext || window.webkitAudioContext 
-var context = new AudioContext()
-var o = context.createOscillator()
-var g = context.createGain()
-g.connect(context.destination)
-g.gain.setValueAtTime(0,context.currentTime)
-o.connect(g)
-o.start(0)
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+var context = new AudioContext();
+var o = context.createOscillator();
+var g = context.createGain();
+g.connect(context.destination);
+g.gain.setValueAtTime(0, context.currentTime);
+o.connect(g);
+o.start(0);
